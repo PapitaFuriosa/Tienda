1d92ab775a9a752fb0a4e9dfc78c63db30ce08f1
Byron Rojas <rojaslencho630@gmail.com>
2026-02-17 20:31:27 -0600

    intento

---
+++ tienda/src/main/java/com/tienda/controller/CategoriaController.java
@@ -0,0 +1,20 @@
+
+package com.tienda.controller;
+
+@Controller
+@RequestMapping("/categoria")
+
+public class CategoriaController {
+
+      @Autowired
+    private CategoriaService categoriaService;
+
+    @GetMapping("/listado")
+    public String inicio(Model model) {
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("categorias", categorias);
+        model.addAttribute("totalCategorias", categorias.size());
+        return "/categoria/listado";
+    }
+    
+}
